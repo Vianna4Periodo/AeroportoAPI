51cc05f7d8c26b53b536fd769bc36b62eb20e0e2
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2018-05-18 15:58:39 -0300

    Criação dos modelos e associações.

---
+++ aerporto-api/api/models/Cliente.js
@@ -0,0 +1,45 @@
+/**
+ * Cliente.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    nome: {
+      type: 'string',
+      maxLength: 200,
+      required: true,
+      isNotEmptyString: true
+    },
+    email: {
+      type: 'string',
+      maxLength: 200,
+      required: true,
+      unique: true,
+      isEmail: true
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    voos: {
+      collection: 'voo',
+      via: 'users'
+    }
+
+  },
+
+};
+
---
+++ aerporto-api/api/models/Voo.js
@@ -0,0 +1,58 @@
+/**
+ * Voo.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    horarioSaida: {
+      type: 'string',
+      required: true,
+      isNotEmptyString: true
+    },
+    horarioChegada: {
+      type: 'string',
+      required: true,
+      isNotEmptyString: true
+    },
+    destino: {
+      type: 'string',
+      required: true,
+      isNotEmptyString: true
+    },
+    quantidadeLugares: {
+      type: 'number',
+      required: true
+    },
+    valor: {
+      type: 'number',
+      columnType: 'double',
+      required: true
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    aeronave: {
+      model: "aeronave"
+    },
+    clientes: {
+      collection: 'cliente',
+      via: 'voos'
+    }
+  },
+
+};
+
