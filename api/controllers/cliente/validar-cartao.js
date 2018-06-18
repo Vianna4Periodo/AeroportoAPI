9ba7eb186155e1b3928c3514e31bcb25fb8644cd
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2018-06-18 08:55:45 -0300

    Dump database

---
+++ api/controllers/cliente/validar-cartao.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'Validar cartao',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    return exits.success();
+
+  }
+
+
+};
