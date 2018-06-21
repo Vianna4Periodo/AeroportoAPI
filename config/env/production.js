4e99d7e3586b17f2e7a2a6eb68515ceca784fe6f
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2018-06-21 08:17:23 -0300

    sdfsdf

--- config/env/production.js
+++ config/env/production.js
@@ -251,10 +251,9 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'https://aeroporto-api.herokuapp.com/',
+    ],
 
 
     /***************************************************************************
