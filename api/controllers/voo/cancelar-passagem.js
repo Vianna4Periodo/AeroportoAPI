733f30ed2bf6c1c1945dbd7262740ccab7770597
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2018-06-21 08:58:18 -0300

    Correcão cancelamento de passagem

--- api/controllers/voo/cancelar-passagem.js
+++ api/controllers/voo/cancelar-passagem.js
@@ -22,9 +22,11 @@ module.exports = {
 
   fn: async function (inputs, exits) {
 
-    await Passagem.destroy({
-      id: inputs.idPassagem
-    });
+    // await Passagem.destroy({
+    //   id: inputs.idPassagem
+    // });
+
+    await Passagem.destroy({ id: inputs.idPassagem });
 
     return exits.success();
 
